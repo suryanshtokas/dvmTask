ce058f2fa7374231a316481b46822522f0b6d95e
Suryansh Tokas <smartonedge@gmail.com>
2026-03-17 01:35:51 +0530

    setup nodes/edges with one way/two way roads

--- network/admin.py
+++ network/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Node, Edge
 
-# Register your models here.
+@admin.register(Node)
+class NodeAdmin(admin.ModelAdmin):
+    list_display = ["id", "name"]
+    search_fields = ["name"]
+
+
+@admin.register(Edge)
+class EdgeAdmin(admin.ModelAdmin):
+    list_display = ["id", "from_node", "to_node"]
+    search_fields = ["from_node__name", "to_node__name"]
+    list_filter = ["from_node", "to_node"]
--- network/models.py
+++ network/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Node(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Edge(models.Model):
+    from_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="outgoing_edges")
+    to_node = models.ForeignKey(Node, on_delete=models.CASCADE, related_name="incoming_edges")
+
+    class Meta:
+        unique_together = ('from_node', 'to_node')
+
+    def __str__(self):
+        return f"{self.from_node} -> {self.to_node}"
