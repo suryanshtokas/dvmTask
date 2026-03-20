e0c5339b34ef6bd3ed84b593bbf8b7fd4e6b4e6b
Suryansh Tokas <smartonedge@gmail.com>
2026-03-21 01:21:09 +0530

    fix CSRF issue with SSL

--- carpooling/settings.py
+++ carpooling/settings.py
@@ -29,6 +29,7 @@ DEBUG = os.environ.get('DEBUG', '1') == '1'
                             
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "localhost 127.0.0.1").split(" ")
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
